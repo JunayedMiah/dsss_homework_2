1ab38ae40c6b4841a0c9f4f8e53e2d09b8fc8350
junay <junayed.miah7667@gmail.com>
2024-11-10 02:04:35 +0100

    make the quiz pip installable

--- math_quiz/__init__.py
+++
@@ -1,2 +0,0 @@
-# math_quiz/__init__.py
-from .math_quiz import math_quiz, generate_random_value, random_operator, create_questions
--- setup.py
+++ setup.py
@@ -1,26 +1,13 @@
-from setuptools import setup, find_packages
+from setuptools import setup
+
+with open("README.md", "r", encoding = "utf-8") as fh:
+    long_description = fh.read()
 
 setup(
-    name="math_quiz",
-    version="0.1",
-    packages=find_packages(),
-    install_requires=[],  # Add any dependencies here if needed
-    entry_points={
-        'console_scripts': [
-            'math_quiz=math_quiz.math_quiz:math_quiz',  # Maps the CLI command to the function
-        ],
-    },
-    author="junay",
-    author_email="junayed.miah7667@gmail.com",
-    description="A math quiz game to test arithmetic skills",
-    long_description=open('README.md').read(),
-    long_description_content_type='text/markdown',
-    url="https://github.com/JunayedMiah/dsss_homework_2.git",
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.12",
-        "License :: OSI Approved :: Apache Software License",
-        "Operating System :: OS Independent",
-    ],
-    python_requires='>=3.12',
+    name = "math_quiz",
+    version = "0.0.1",
+    author = "junay",
+    license = "Apache 2.0",
+    long_description = long_description,
+    url = "https://github.com/JunayedMiah/dsss_homework_2.git"
 )
